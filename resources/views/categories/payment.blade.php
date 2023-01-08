735058050c90f1ad3019960ffccc2dcd38c65816
Mohamed Elshamy <mohamedabdelhady88@gmail.com>
2023-01-08 20:19:33 +0200

    .

--- resources/views/categories/payment.blade.php
+++ resources/views/categories/payment.blade.php
@@ -1,46 +1,51 @@
-@extends("layouts.app")
+@extends('layouts.app')
 
 
-@section("content")
-<div class="containerrr d-lg-flex">
-
-    <div class="box-2">
-        <div class="box-inner-2">
-            <div>
-                <p class="paymentdetails"> <b>Payment Details</b></p>
-                <p class="paymentpara">Complete your purchase by providing your payment details.</p><br>
-            </div>
-
-         <form action="{{route("client.payment")}}" method="POST">
-            @csrf
-            <div>
-                    <p class="paymentlabel">Please select your payment method</p>
-                    <input type="radio" id="v1"  name="payment_method" value="Visa">
-                    <label class="thepaymentmethod" for="v1">Visa payment</label><br>
-                    <input type="radio" id="c1"  name="payment_method" value="cash">
-                    <label class="thepaymentmethod" for="c1">Cash</label><br>
-
-            </div>
+@section('content')
+    <div class="containerrr d-lg-flex">
 
 
-            <div id="cardDetails">
-                    <p class="carddetails">Card details</p>
+        <div class="box-2">
+            <div class="box-inner-2">
+                <div>
+                    <p class="paymentdetails"> <b>Payment Details</b></p>
+                    <p class="paymentpara">Complete your purchase by providing your payment details.</p><br>
+                </div>
 
-                    <div class="thecarddetails">
-                            {{-- <form action="" onsubmit="check()"> --}}
-                        Card Number <input type="text" id="cardnum"  class="form-control"  placeholder="0000-0000-0000-0000" required> <br>
-                        EXP    <input type="text" maxlength=5 class="form-control"  id="exp" placeholder="07-09" required><br>
-                        CVV   <input type="text" maxlength=3 class="form-control" id="cvv"  placeholder="523" required><br>
-                        Cardholder Name <input class="form-control" onclick="checkname()"  type="text" id="name" placeholder="John Adams" required>
-                        <input type="submit" class="paybutton" value="Save">
-
-
+                <form action="{{ route('client.payment') }}" method="POST">
+                    @csrf
+                    <div>
+                        <p class="paymentlabel">Please select your payment method</p>
+                        <input type="radio" id="v1" name="payment_method" value="Visa">
+                        <label class="thepaymentmethod" for="v1">Visa payment</label><br>
+                        <input type="radio" id="c1" name="payment_method" value="cash">
+                        <label class="thepaymentmethod" for="c1">Cash</label><br>
 
                     </div>
-            </div>
-        </form>
-        <form action="">
-            @csrf
+
+
+                    <div id="cardDetails">
+                        <p class="carddetails">Card details</p>
+
+                        <div class="thecarddetails">
+                            {{-- <form action="" onsubmit="check()"> --}}
+                            Card Number <input type="text" id="cardnum" class="form-control"
+                                placeholder="0000-0000-0000-0000" required> <br>
+                            EXP <input type="text" maxlength=5 class="form-control" id="exp" placeholder="07-09"
+                                required><br>
+                            CVV <input type="text" maxlength=3 class="form-control" id="cvv" placeholder="523"
+                                required><br>
+                            Cardholder Name <input class="form-control" onclick="checkname()" type="text" id="name"
+                                placeholder="John Adams" required>
+                            <input type="submit" class="paybutton" value="Save">
+
+
+
+                        </div>
+                    </div>
+                </form>
+                <form action="">
+                    @csrf
 
                     <div class="address">
                         <p class="billingadress">Billing address</p>
@@ -55,66 +60,86 @@
                         <div class="d-flex">
 
                             <input class="form-control zip" type="text" placeholder="street">
-                            <input class="form-control state" id="bnum" type="text" maxlength=3 placeholder="buildingNo" onclick="checkaddr()" required>
+                            <input class="form-control state" id="bnum" type="text" maxlength=3
+                                placeholder="buildingNo" onclick="checkaddr()" required>
                             <input type="submit" class="paybutton" value="Save Adress">
                         </div>
                     </div>
-        </form>
-        <div>
-            <form action="">
-                @csrf
-
+                </form>
                 <div>
-                    <h6 class="order"><b>Order recap</b></h6>
-                    <table>
-                        <tr>
-                            <td >Subtotal</td>
-                            <td align="right"> $225</td>
-                        </tr>
-                        <tr>
-                            <td >Vat(20%)</td>
-                            <td align="right"> $25</td>
-                        </tr>
-                        <tr>
-                            <td >Shippingfee</td>
-                            <td align="right"> $5</td>
-                        </tr>
-                        <tfoot>
-                        <tr>
-                            <td width="25%">Total</td>
-                            <td align="right">$205</td>
-                        </tr>
-                    </tfoot>
-                    </table>
+                    <form action="">
+                        @csrf
+
+                        <div>
+                            <h6 class="order"><b>Order recap</b></h6>
+                            <table>
+                                <tr>
+                                    <td>Subtotal</td>
+                                    <td align="right"> $225</td>
+                                </tr>
+                                <tr>
+                                    <td>Vat(20%)</td>
+                                    <td align="right"> $25</td>
+                                </tr>
+                                <tr>
+                                    <td>Shippingfee</td>
+                                    <td align="right"> $5</td>
+                                </tr>
+                                <tfoot>
+                                    <tr>
+                                        <td width="25%">Total</td>
+                                        <td align="right">$205</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <input type="submit" class="paybutton" value="Place your order">
+                    </form>
                 </div>
-                <input type="submit" class="paybutton" value="Place your order">
-            </form>
+
+
+                <div id="myRadioGroup">
+                    2 Cars<input type="radio" name="cars" checked="checked" value="2" />
+                    3 Cars<input type="radio" name="cars" value="3" />
+
+                    <div id="Cars2" class="desc">
+                        2 Cars Selected
+                    </div>
+                    <div id="Cars3" class="desc" style="display: none;">
+                        3 Cars
+                    </div>
+                </div>
+
+
+
+            </div>
+
+            {{-- </div> --}}
         </div>
-
-
-
-
-
-
-        </div>
-
-        {{-- </div> --}}
     </div>
-</div>
 
-<script>
-    function display()
-    {
-      var checkRadio=document.querySelector('input[name="payment_method"]:checked') ;
-      if(checkRadio!=null)
-      {
-        document.getElementby
-      }
-    }
+    <script>
+        // function display() {
+        //     var checkRadio = document.querySelector('input[name="payment_method"]:checked');
+        //     if (checkRadio != null) {
+        //         document.getElementby
+        //     }
+        // }
+        $(document).ready(function() {
+            $("#cardDetails").hide();
+            $("input[name$='payment_method']").click(function() {
+                var test = $(this).val();
+                console.log(test);
+                if (test == "Visa") {
+                    $("#cardDetails").show();
 
+                } else if (test == "cash") {
+                    $("#cardDetails").hide();
 
-</script>
-
-
-
+                }
+                // $("div.desc").hide();
+                // $("#cardDetails").show();
+            });
+        });
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        {{-- @include("auth.css") --}}
-        <link rel="stylesheet" href={{ asset('/css/stylesheet.css')}}>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- @include("auth.css") --}}
+    <link rel="stylesheet" href={{ asset('/css/stylesheet.css') }}>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-        <!-- Scripts -->
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <script src="https://kit.fontawesome.com/3929e16ef5.js" crossorigin="anonymous"></script>
-        <script src="{% static 'network/functions.js' %}"></script>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+
+    <script src="https://kit.fontawesome.com/3929e16ef5.js" crossorigin="anonymous"></script>
+    <script src="{% static 'network/functions.js' %}"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+</head>
+
+<body class="font-sans antialiased">
+    <div class="">
+        @include('auth.navbar')
+        {{-- @include("categories.navbar_test") --}}
 
 
-    </head>
-    <body class="font-sans antialiased" >
-        <div class="">
-            @include("auth.navbar")
-            {{-- @include("categories.navbar_test") --}}
+        {{-- @include('layouts.navigation') --}}
 
-
-            {{-- @include('layouts.navigation') --}}
-
-            <!-- Page Heading -->
-            {{-- @if (isset($header))
+        <!-- Page Heading -->
+        {{-- @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
@@ -40,23 +44,34 @@
                 </header>
             @endif --}}
 
-            <!-- Page Content -->
-            <main>
+        <!-- Page Content -->
+        <main>
 
-               @yield("content")
-            </main>
-            @include('auth.footer')
-        </div>
-        <script src="./js/jquery-1.9.1.js"></script>
-        <!--script rakam 1000-->
-        <script src="./js/bootstrap-dropdown.js"></script>
+            @yield('content')
+        </main>
+        @include('auth.footer')
+    </div>
+    <script src="./js/jquery-1.9.1.js"></script>
+    <!--script rakam 1000-->
+    <script src="./js/bootstrap-dropdown.js"></script>
 
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script src="./js/bootstrap.js"></script>
-        <script src="{% static 'js/bootstrap.js'%}"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+    <script src="./js/bootstrap.js"></script>
+    <script src="{% static 'js/bootstrap.js'%}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
