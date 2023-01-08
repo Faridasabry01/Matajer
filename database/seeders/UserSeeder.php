1dea7fe4d74fb9bb91587f005347279bd7ed8d34
Mohamed Elshamy <mohamedabdelhady88@gmail.com>
2023-01-08 16:18:19 +0200

    user seeder.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,20 +18,11 @@ class UserSeeder extends Seeder
     {
         // User::factory()->count(50)->create();
         User::create([
-<<<<<<< Updated upstream
-            'name'=>'farida',
-            'email'=>'ferfer1122011@gmail.com',
-            'email_verified_at'=>now(),
-            'phone_number'=>'0123456789',
-            'password'=>Hash::make('123123123')
-            ]);
-=======
             'name' => 'farida',
             'email' => 'ferfer1122011@gmail.com',
             'email_verified_at' => now(),
             'phone_number' => '0123456789',
-            'password' => Hash::make('123123123'),
+            'password' => Hash::make('123123123')
         ]);
->>>>>>> Stashed changes
     }
 }
