b21f820eec5936732562e5292296dc80bfd37541
Daliayasser1 <daliakb2161@gmail.com>
2023-01-09 22:29:29 +0200

    .

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -19,11 +19,11 @@
             @csrf
             @method('delete')
 
-            <h2 class="text-lg font-medium text-gray-900">
+            <h2 class="areyousure">
                 {{ __('Are you sure you want to delete your account?') }}
             </h2>
 
-            <p class="mt-1 text-sm text-gray-600">
+            <p class="makesure">
                 {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
             </p>
 
@@ -41,12 +41,12 @@
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
             </div>
 
-            <div class="mt-6 flex justify-end">
-                <x-secondary-button x-on:click="$dispatch('close')">
+            <div>
+                <x-secondary-button class="cancelbutton" x-on:click="$dispatch('close')">
                     {{ __('Cancel') }}
                 </x-secondary-button>
 
-                <x-danger-button class="ml-3">
+                <x-danger-button class="okdelete">
                     {{ __('Delete Account') }}
                 </x-danger-button>
             </div>
